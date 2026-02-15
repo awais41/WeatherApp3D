db898eb8f2a4ef172c93775d076a14aff6ec16ef
Awais <37880368+awais41@users.noreply.github.com>
2026-02-15 19:49:25 +0500

    Update api_constants.dart

--- lib/lib/core/constants/api_constants.dart
+++ lib/lib/core/constants/api_constants.dart
@@ -1,5 +1,5 @@
 class ApiConstants {
-  static const String baseUrl = 'https://weathernodeserver.onrender.com';
+  static const String baseUrl = 'https://';
   static const String weatherEndpoint = '/weather';
   // If your server later requires an API key, add it here and include in requests.
 }
